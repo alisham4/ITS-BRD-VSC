fa1cb65e97555692a3112e2d5ac75f8b57a0257f
Eleonora Nuzhdina <wgz223@haw-hamburg.de>
2025-10-24 13:30:57 +0200

    calculator modul hinzugefügt

--- Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp/Stack.Debug+ITSboard-configure-.cmake
+++ Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp/Stack.Debug+ITSboard-configure-.cmake
@@ -1,11 +1,11 @@
 
 cmake_minimum_required(VERSION 3.15)
 
-set(command "/Users/alishamuzaffar/.vcpkg/artifacts/2139c4c6/tools.kitware.cmake/3.28.4/bin/cmake;-G;Ninja;-S;/Users/alishamuzaffar/GitProjekte/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard;-B;/Users/alishamuzaffar/GitProjekte/ITS-BRD-VSC/Programs/Stack/tmp/1;-DSOLUTION_ROOT=/Users/alishamuzaffar/GitProjekte/ITS-BRD-VSC/Programs/Stack;-DCMSIS_PACK_ROOT=/Users/alishamuzaffar/.cache/arm/packs;-DCMSIS_COMPILER_ROOT=/Users/alishamuzaffar/.vcpkg/artifacts/2139c4c6/tools.open.cmsis.pack.cmsis.toolbox/2.11.0/etc")
+set(command "C:/Users/enuzh/.vcpkg/artifacts/2139c4c6/tools.kitware.cmake/3.28.4/bin/cmake.exe;-G;Ninja;-S;C:/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard;-B;C:/ITS-BRD-VSC/Programs/Stack/tmp/1;-DSOLUTION_ROOT=C:/ITS-BRD-VSC/Programs/Stack;-DCMSIS_PACK_ROOT=C:/Users/enuzh/AppData/Local/arm/packs;-DCMSIS_COMPILER_ROOT=C:/Users/enuzh/.vcpkg/artifacts/2139c4c6/tools.open.cmsis.pack.cmsis.toolbox/2.11.0/etc")
 set(log_merged "")
 set(log_output_on_failure "ON")
-set(stdout_log "/Users/alishamuzaffar/GitProjekte/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp/Stack.Debug+ITSboard-configure-out.log")
-set(stderr_log "/Users/alishamuzaffar/GitProjekte/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp/Stack.Debug+ITSboard-configure-err.log")
+set(stdout_log "C:/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp/Stack.Debug+ITSboard-configure-out.log")
+set(stderr_log "C:/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp/Stack.Debug+ITSboard-configure-err.log")
 execute_process(
   COMMAND ${command}
   RESULT_VARIABLE result
@@ -31,7 +31,7 @@ if(result)
   if (${log_merged})
     set(msg "${msg}\nSee also\n  ${stderr_log}")
   else()
-    set(msg "${msg}\nSee also\n  /Users/alishamuzaffar/GitProjekte/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp/Stack.Debug+ITSboard-configure-*.log")
+    set(msg "${msg}\nSee also\n  C:/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp/Stack.Debug+ITSboard-configure-*.log")
   endif()
   if (${log_output_on_failure})
     message(SEND_ERROR "${msg}")
@@ -50,7 +50,7 @@ if(result)
   endif()
 else()
   if(NOT "Ninja" MATCHES "Ninja")
-    set(msg "Stack.Debug+ITSboard configure command succeeded.  See also /Users/alishamuzaffar/GitProjekte/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp/Stack.Debug+ITSboard-configure-*.log")
+    set(msg "Stack.Debug+ITSboard configure command succeeded.  See also C:/ITS-BRD-VSC/Programs/Stack/tmp/Stack.Debug+ITSboard/src/Stack.Debug+ITSboard-stamp/Stack.Debug+ITSboard-configure-*.log")
     message(STATUS "${msg}")
   endif()
 endif()
