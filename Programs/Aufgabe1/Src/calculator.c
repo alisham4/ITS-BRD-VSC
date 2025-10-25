8ff0434f4d1f8ee7f4c64e8553fd2fbbda2d4d55
Eleonora Nuzhdina <wgz223@haw-hamburg.de>
2025-10-25 12:49:59 +0200

    arithmetische operationen hinzugefügt

--- Programs/Aufgabe1/Src/calculator.c
+++ Programs/Aufgabe1/Src/calculator.c
@@ -1,3 +1,30 @@
 #include "calculator.h"
+#include "stack.h"
 
+int add(void){
+    int number1 = pop();
+    int number2 = pop();
+    int answer = number1+number2;
+    push(answer);
+}
 
+int sub(void){
+    int number1 = pop();
+    int number2 = pop();
+    int answer = number1-number2;
+    push(answer);
+}
+
+int mul(void){
+    int number1 = pop();
+    int number2 = pop();
+    int answer = number1*number2;
+    push(answer);
+}
+
+int div(void){
+    int number1 = pop();
+    int number2 = pop();
+    int answer = number1/number2;
+    push(answer);
+}
