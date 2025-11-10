76a3284b4f206ca5b873484e6fba725e7f3548b6
Alisha Muzaffar <alisha.muzaffar>
2025-11-10 19:20:46 +0100

    fehler in main.h

---
+++ Programs/Aufgabe2/Inc/leds.h
@@ -0,0 +1,8 @@
+#ifndef LEDS_H
+#define LEDS_H
+
+#define LED_FORWARD D32
+#define LED_BACKWARD D22
+#define LED_ERROR D21
+
+#endif
---
+++ Programs/Aufgabe2/Inc/pins.h
@@ -0,0 +1,6 @@
+#ifndef PINS_H
+#define PINS_H
+
+#define PIN_A IN0
+#define PIN_B IN1
+#endif
